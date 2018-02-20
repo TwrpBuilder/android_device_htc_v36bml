8eaa706b7441ba5b08604a2d5bc14c8c13eb4472
kirito96 <aquammie96@gmail.com>
2018-02-20 12:59:10 -0400

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/htc/htc_v36bml_dugl
+
+TARGET_BOARD_PLATFORM := mt6753
+TARGET_BOOTLOADER_BOARD_NAME := htc_v36bml_dugl
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432 
+BOARD_FLASH_BLOCK_SIZE := 1000000
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_EXCLUDE_SUPERSU := true
+include $(LOCAL_PATH)/kernel.mk
+include device/htc/htc_v36bml_dugl/BoardConfig64.mk
+TARGET_RECOVERY_FSTAB := device/htc/htc_v36bml_dugl/recovery.fstab
